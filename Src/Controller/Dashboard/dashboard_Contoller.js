a54f6b2c0a51dafadd289ce5008ced2ffd7620cf
Natnael Alemayehu <natnael@triaplc.com>
2023-10-13 16:15:48 +0300

    **feat     ** added routes for saving data and reading data ,     ** added file based data management

--- Src/Controller/Dashboard/dashboard_Contoller.js
+++ Src/Controller/Dashboard/dashboard_Contoller.js
@@ -1,10 +1,53 @@
+import * as fs from "fs/promises";
+
 export class DashBoard_Controller
 {
     static SaveDashboard()
     {
         return async(req,res,next) => {
 
-            res.send(200).send("Route In development")
+            const {dashboard} = req.body;
+
+            console.log(dashboard);
+
+            try {
+                 
+                
+               fs.readFile('./Src/Utils/SavedDashBoards.json',{"encoding" : "utf8"}).then(data => {
+                    return JSON.parse(data);
+               }).catch(err => {
+                    console.log("the error on read " , err);
+                    return [];
+               }).then( (data) => {
+                        console.log("the saved Data " , data);
+                        data.push( { name : `Dashboard ${data.length + 1}` ,  value: [...dashboard]});
+                        console.log("the new Data to be saved " , data);
+                       
+                        fs.writeFile('./Src/Utils/SavedDashBoards.json', JSON.stringify(data) ,{"encoding" : "utf8" , "flag" : "w+" })
+                        .then( () => { res.status(200).json({res : "new dashboard  was saved"})} )
+                        .catch(err => {  console.log("the new Data failed to be saved " , err);  res.status(500).json({res : "new dashboard  failed to save" , error : err}) });
+               });
+
+
+               /*  console.log("the saved Data " , data);
+                data.push(...dashboard);
+                console.log("the new Data to be saved " , data);
+
+                fs.writeFile('./Utils/SavedDashBoards.json', JSON.stringify(data) ,{"encoding" : "utf8" , "flag" : "w+" })
+                .then( () => { res.status(200).json({res : "new dashboard  was saved"})} );
+                
+                fs.readFile('./Utils/SavedDashBoards.json',{"encoding" : "utf8"}).then(data => {
+                    console.log("the saved Data " , data);
+                    data.push(...dashboard);
+                    console.log("the new Data to be saved " , data);
+                 
+                }).catch(err => { res.status(200).json({res : "new dashboard  failed to save" , error : err}) }) */
+              } catch (err) {
+                console.error(err);
+                res.status(500).json({error : err}); 
+              }
+
+            
         }
     }
 
@@ -12,8 +55,20 @@ export class DashBoard_Controller
     static ReadDashboard()
     {
         return async(req,res,next) => {
-
-            res.send(200).send("Route In development")
+            try {
+                
+            fs.readFile('./Src/Utils/SavedDashBoards.json',{"encoding" : "utf8"})
+            .then(data => {
+                    const dashboardsData =  JSON.parse(data);
+                    res.status(200).json({result : dashboardsData});
+            }).catch(err => {
+                    console.log("the error on read " , err);
+                    res.status(200).json({result : []})})
+            } catch (err) {
+                 console.error(err);
+                 res.status(500).json({result : [] ,  error : err}); 
+               }
+           
         }
     }
 }
--- Src/Routes/router.js
+++ Src/Routes/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ReportType_Controller } from "../Controller/Reports/reportType_Controller.js";
 import { ReportData_Controller } from "../Controller/Reports/reportData_Controller.js";
+import { DashBoard_Controller } from "../Controller/Dashboard/dashboard_Contoller.js";
 
 
 
@@ -11,6 +12,10 @@ router.get("/reports",ReportType_Controller.ListProductsWithSubs());
 router.post("/reports/attribs",ReportType_Controller.ListAttributes());
 
 router.post("/reports/data",ReportData_Controller.ReadData());
+
+router.post("/dashboards/save",DashBoard_Controller.SaveDashboard());
+
+router.get("/dashboards/read",DashBoard_Controller.ReadDashboard());
 
 
 export {router};
