f37f360141fcb22820fa8b3b0e3221aec52d1725
Natnael Alemayehu <natnael@triaplc.com>
2023-10-13 10:57:33 +0300

    ** feat     ** added chart query route to handle chart data configurations from base data  ,

--- Src/Routes/router.js
+++ Src/Routes/router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ReportType_Controller } from "../Controller/Reports/reportType_Controller.js";
+import { ReportData_Controller } from "../Controller/Reports/reportData_Controller.js";
 
 
 
@@ -8,6 +9,8 @@ const router = Router();
 router.get("/reports",ReportType_Controller.ListProductsWithSubs());
 
 router.post("/reports/attribs",ReportType_Controller.ListAttributes());
+
+router.post("/reports/data",ReportData_Controller.ReadData());
 
 
 export {router};
